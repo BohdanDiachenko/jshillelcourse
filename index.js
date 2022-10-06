d2aa22f91721c1fbf9600f208067d259475f161f
wBogdaNw <bogdanid91@gmail.com>
2022-10-06 20:06:54 +0200

    hw6

---
+++ index.js
@@ -0,0 +1,5 @@
+let firstNumber = +prompt('введіть перше число');
+let secondNumber = +prompt('введіть друге число');
+let thirdNumber = +prompt('введіть третє число');
+let arithmeticMean = `${(firstNumber+secondNumber+thirdNumber)/3}`;
+alert(arithmeticMean);
