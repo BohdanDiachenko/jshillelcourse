59e62293e350ef5a891e6040a93e19236291e0ba
wBogdaNw <bogdanid91@gmail.com>
2022-10-15 11:02:50 +0200

    hw12

--- index.js
+++ index.js
@@ -1,163 +1,75 @@
 
-let cityOfResidence;
-let sportLegend;
-let currentYear = 2022;
-let answer;
+// Дан масив об'єктів. Вивести масив телефонних номерів користувачів, у яких баланс більше 2000 доларів. І знайти суму всіх балансів користувачівlet users = [
+    {
+    "index": 0,
+    "isActive": true,
+    "balance": "$2,226.60",
+    "name": "Eugenia Sawyer",
+    "gender": "female",
+    "phone": "+1 (840) 583-3207",
+    "address": "949 John Street, Rose, Puerto Rico, 1857"
+    },
+    {
+    "index": 1,
+    "isActive": true,
+    "balance": "$2,613.77",
+    "name": "Pauline Gallegos",
+    "gender": "female",
+    "phone": "+1 (985) 593-3328",
+    "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+    },
+    {
+    "index": 2,
+    "isActive": false,
+    "balance": "$3,976.41",
+    "name": "Middleton Chaney",
+    "gender": "male",
+    "phone": "+1 (995) 591-2478",
+    "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
+    },
+    {
+    "index": 3,
+    "isActive": true,
+    "balance": "$1,934.58",
+    "name": "Burns Poole",
+    "gender": "male",
+    "phone": "+1 (885) 559-3422",
+    "address": "730 Seba Avenue, Osage, Alabama, 6290"
+    },
+    {
+    "index": 4,
+    "isActive": true,
+    "balance": "$3,261.65",
+    "name": "Mcfadden Horne",
+    "gender": "male",
+    "phone": "+1 (942) 565-3988",
+    "address": "120 Scholes Street, Kirk, Michigan, 1018"
+    },
+    {
+    "index": 5,
+    "isActive": false,
+    "balance": "$1,790.56",
+    "name": "Suzette Lewis",
+    "gender": "female",
+    "phone": "+1 (837) 586-3283",
+    "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+    }
+]
+const getArrayUserPhone = (users) => {
+    return users.reduce((userPhone, user) =>{
+        if(Number(user.balance.replace('$','').replace(',', '')) >= 2000){
+            userPhone.push(user.phone)
+        }
+        return userPhone
+    }, [])
+}
+console.log(getArrayUserPhone(users));
 
-let getAge = +prompt('Введіть рік вашого народження');
-if (getAge === 0){
-    alert('Шкода, що Ви не захотіли ввести свій рік народження')
-};
+const getArrayUserBalance = (users) => {
+    return users.reduce((userBalance, user) =>{
+            userBalance += Number(user.balance.replace('$','').replace(',', ''))
+        return (Math.round(userBalance * 100) / 100)  
+    }, 0)
+}
 
-let getResidence = prompt('Введіть ваше місце проживання');
-if (getResidence === null){
-    alert('Шкода, що Ви не захотіли ввести свіє місце проживання');
-};
-
-let getKindOfSport = prompt('Введіть ваш улюблений вид спорту');
-if (getKindOfSport === null){
-    alert('Шкода, що Ви не захотіли ввести свій улюбленний вид спорту');
-};
-
-switch (getResidence) {
-    case 'Київ':
-        cityOfResidence = 'Ти живеш у столиці України\n';
-        break;
-    case 'Вашингтон':
-        cityOfResidence = 'Ти живеш у столиці Сполучених Штатів Америки\n';
-        break;
-    case 'Лондон':
-        cityOfResidence = 'Ти живеш у столиці Англії\n';
-        break;
-    case null:
-        cityOfResidence = '';
-        break;
-
-    default: cityOfResidence = `Ти живеш місті ${getResidence}\n`;
-        break;
-};
-switch (getKindOfSport) {
-    case 'футбол':
-        sportLegend = 'Круто! Хочеш стати як Андрій Шевченко?';
-        break;
-    case 'баскетбол':
-        sportLegend = 'Круто! Хочеш стати як Майкл Джордан?';
-        break;
-    case 'теніс':
-        sportLegend = 'Круто! Хочеш стати як Роджер Федерер?';
-        break;
-    case null:
-        sportLegend = '';
-        break;
-
-    default: sportLegend = '';
-        break;
-};
-
-answer = (getAge ? `Ваш вік ${currentYear - getAge}.\n` : '') +  `${cityOfResidence}${sportLegend}`;
-
-if(getAge || cityOfResidence || sportLegend){
-    alert(answer);
-};
-
-
-// let arithmeticOperations = prompt('що ви хочете зробити(add, sub, mult, div)?');
-// let result;
-// if(arithmeticOperations === 'add' || arithmeticOperations === 'sub' || arithmeticOperations === 'mult' || arithmeticOperations === 'div'){
-//     let oneNumber = +(prompt('введіть перше число'));
-//     let twoNumber = +(prompt('введіть друге число'));
-//     if(arithmeticOperations === 'add'){
-//         arithmeticOperations = '+';
-//         result = oneNumber + twoNumber;
-//     };
-//     if (arithmeticOperations === 'sub'){
-//         arithmeticOperations = '-';
-//         result = oneNumber - twoNumber;
-//     };
-//     if (arithmeticOperations === 'mult'){
-//         arithmeticOperations = '*';
-//         result = oneNumber * twoNumber;
-//     };  
-//     if (arithmeticOperations === 'div'){
-//         arithmeticOperations = '/';
-//         result = oneNumber / twoNumber;
-//     };
-//     alert(`${oneNumber} ${arithmeticOperations} ${twoNumber} = ${result}`);
-// } 
-// else {
-//     alert('читай умову');
-// };
-
-
-
-// let arithmeticOperations;
-// let oneNumber;
-// let twoNumber
-// switch (arithmeticOperations = prompt('що ви хочете зробити(add, sub, mult, div)?')) {
-
-//     case a = "add":
-//         oneNumber = +(prompt('введіть перше число'));
-//         twoNumber = +(prompt('введіть друге число'));
-//         alert(`${oneNumber} + ${twoNumber} = ${oneNumber + twoNumber}`);
-//         break;
-
-//     case "sub":
-//         oneNumber = +(prompt('введіть перше число'));
-//         twoNumber = +(prompt('введіть друге число'));
-//         alert(`${oneNumber} - ${twoNumber} = ${oneNumber - twoNumber}`);
-//         break;
-
-//     case "mult":
-//         oneNumber = +(prompt('введіть перше число'));
-//         twoNumber = +(prompt('введіть друге число'));
-//         alert(`${oneNumber} * ${twoNumber} = ${oneNumber * twoNumber}`);;
-//         break;
-
-//     case "div":
-//         oneNumber = +(prompt('введіть перше число'));
-//         twoNumber = +(prompt('введіть друге число'));
-//         alert(`${oneNumber} / ${twoNumber} = ${oneNumber / twoNumber}`);;
-//         break;
-        
-//     default:
-//         alert("Читай умову!");
-//         break;
-// };
-
-// let firstNumber = +prompt('введіть перше число');
-// let secondNumber = +prompt('введіть друге число');
-// let add = `${firstNumber}+${secondNumber} = ${firstNumber + secondNumber}`;
-// let sub = `${firstNumber}-${secondNumber} = ${firstNumber - secondNumber}`;
-// let mult = `${firstNumber}*${secondNumber} = ${firstNumber * secondNumber}`;
-// let div = `${firstNumber}/${secondNumber} = ${firstNumber / secondNumber}`;
-// let resolt = `Ваш результат:\n${add}\n${sub}\n${mult}\n${div}`;
-// alert(resolt);
-
-
-//homework2/1
-
-// let surName = prompt ('Enter your last name');
-
-// let firstName = prompt('Enter your firs name');
-
-// let patronymic = prompt('Enter your middle name');
-
-// let greeting = 'good morning ' + surName + ' ' + firstName + ' ' + patronymic;
-// // let greeting = `Good Morning ${surName} ${firstName} ${patronymic}`;
-
-
-// alert(greeting);
-
-
-
-
-// HomeWork2/2
-
-// let number =  92358;
-// let string = String(number);
-// let newString = '';
-// for(i = 0; i < string.length ; i++){
-//     newString += `${string[i]} `;
-// };
-// console.log(newString);
-
+console.log(getArrayUserBalance(users));
