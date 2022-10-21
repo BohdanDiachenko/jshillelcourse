14711737a530efcc3bed56ad0e4ace06276139c1
wBogdaNw <bogdanid91@gmail.com>
2022-10-21 16:30:22 +0200

    hw15

--- index.js
+++ index.js
@@ -1,168 +1,101 @@
+// -Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).
+// Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак виходять від користувача.
 
-let numOrStr = prompt("input number or string");
-console.log(numOrStr);
+let getMathOperator = prompt('введіть математичний оператор:  +, -, *, /, %, ^');
+let x = +prompt("Введіть перше число");
+let y;
+getMathOperator === '%' ? y = +prompt('Введіть число процент якого від першого числа ви хочете дізнатися') : y = +prompt('Введіть друге число');
 
-switch (isNaN(numOrStr) || numOrStr) {
-    case null:
-        console.log("ви скасували");
-
-        break;
-
-    case "":
-        console.log("Empty String");
-
-        break;
-
-    case true:
-        console.log(" number is Ba_NaN");
-
-        break;
-
-    default:
-        console.log("OK!");
-
-        break;
-}
-
-
-// let arithmeticOperations = prompt('що ви хочете зробити(add, sub, mult, div)?');
-// let result;
-// if(arithmeticOperations === 'add' || arithmeticOperations === 'sub' || arithmeticOperations === 'mult' || arithmeticOperations === 'div'){
-//     let oneNumber = +(prompt('введіть перше число'));
-//     let twoNumber = +(prompt('введіть друге число'));
-//     if(arithmeticOperations === 'add'){
-//         arithmeticOperations = '+';
-//         result = oneNumber + twoNumber;
-//     };
-//     if (arithmeticOperations === 'sub'){
-//         arithmeticOperations = '-';
-//         result = oneNumber - twoNumber;
-//     };
-//     if (arithmeticOperations === 'mult'){
-//         arithmeticOperations = '*';
-//         result = oneNumber * twoNumber;
-//     };  
-//     if (arithmeticOperations === 'div'){
-//         arithmeticOperations = '/';
-//         result = oneNumber / twoNumber;
-//     };
-//     alert(`${oneNumber} ${arithmeticOperations} ${twoNumber} = ${result}`);
-// } 
-// else {
-//     alert('читай умову');
-// };
-
-
-
-// let arithmeticOperations;
-// let oneNumber;
-// let twoNumber
-// switch (arithmeticOperations = prompt('що ви хочете зробити(add, sub, mult, div)?')) {
-
-//     case a = "add":
-//         oneNumber = +(prompt('введіть перше число'));
-//         twoNumber = +(prompt('введіть друге число'));
-//         alert(`${oneNumber} + ${twoNumber} = ${oneNumber + twoNumber}`);
-//         break;
-
-//     case "sub":
-//         oneNumber = +(prompt('введіть перше число'));
-//         twoNumber = +(prompt('введіть друге число'));
-//         alert(`${oneNumber} - ${twoNumber} = ${oneNumber - twoNumber}`);
-//         break;
-
-//     case "mult":
-//         oneNumber = +(prompt('введіть перше число'));
-//         twoNumber = +(prompt('введіть друге число'));
-//         alert(`${oneNumber} * ${twoNumber} = ${oneNumber * twoNumber}`);;
-//         break;
-
-//     case "div":
-//         oneNumber = +(prompt('введіть перше число'));
-//         twoNumber = +(prompt('введіть друге число'));
-//         alert(`${oneNumber} / ${twoNumber} = ${oneNumber / twoNumber}`);;
-//         break;
-        
-//     default:
-//         alert("Читай умову!");
-//         break;
-// };
-
-// let firstNumber = +prompt('введіть перше число');
-// let secondNumber = +prompt('введіть друге число');
-// let add = `${firstNumber}+${secondNumber} = ${firstNumber + secondNumber}`;
-// let sub = `${firstNumber}-${secondNumber} = ${firstNumber - secondNumber}`;
-// let mult = `${firstNumber}*${secondNumber} = ${firstNumber * secondNumber}`;
-// let div = `${firstNumber}/${secondNumber} = ${firstNumber / secondNumber}`;
-// let resolt = `Ваш результат:\n${add}\n${sub}\n${mult}\n${div}`;
-// alert(resolt);
-
-
-//homework2/1
-
-// let surName = prompt ('Enter your last name');
-
-// let firstName = prompt('Enter your firs name');
-
-// let patronymic = prompt('Enter your middle name');
-
-// let greeting = 'good morning ' + surName + ' ' + firstName + ' ' + patronymic;
-// // let greeting = `Good Morning ${surName} ${firstName} ${patronymic}`;
-
-
-// alert(greeting);
-
-
-
-
-// HomeWork2/2
-
-// let number =  92358;
-// let string = String(number);
-// let newString = '';
-// for(i = 0; i < string.length ; i++){
-//     newString += `${string[i]} `;
-// };
-// console.log(newString);
-=======
-let firstNumber = +prompt('введіть перше число');
-let secondNumber = +prompt('введіть друге число');
-let thirdNumber = +prompt('введіть третє число');
-let arithmeticMean = `${(firstNumber+secondNumber+thirdNumber)/3}`;
-alert(arithmeticMean);
-
-const SECONDS_IN_HOUR = 3600;
-let hours = +prompt('введіть кількість годин які хочете перевести в секунди');
-let seconds = `${hours * SECONDS_IN_HOUR} секунд`;
-alert(seconds);
-
-let condition = false;
-let getUserLengthArray;
-do {
-    getUserLengthArray = prompt("Введіть довжину масиву");
-    if (isNaN(getUserLengthArray) === true) {
-        //return "Please imput a number.";
-        alert("Будь ласка, введіть число");
-        condition = false;
-    } else {
-        condition = true;
+const mathOperation = function doMath(x, znak, y) {
+    switch (znak){
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            alert(`${x} ${znak} ${y} = `+ eval(`${x} ${znak} ${y}`));
+            return 
+        case '%':
+            alert(`${(y * 100)/x}%`);
+            return 
+        case '^':
+            alert(`${x**y}`);
+            return 
+        default:
+            alert('Ви ввели невірний оператор');
+            return
     }
-} while (condition === false);
-
-const newArray = [];
-for (let i = 0; i < getUserLengthArray; i++) {
-    newArray[i] = prompt(`Введіть елемент масиву № ${i + 1}`);
-    if (newArray[i] === "" || newArray[i] === null) {
-        delete newArray[i];
-    } else if (!isNaN(+newArray[i])) {
-        newArray[i] = Number(newArray[i]);
-    }
-}
-
-console.log(newArray);
-console.log(newArray.sort().sort((a, b) => a - b));
-newArray.splice(1, 3);
-console.log(newArray);
+};
+let result = mathOperation(x, getMathOperator, y);
 
 
 
+
+// -Дан масив з елементами різних типів. Створити функцію яка вираховує
+//  середнє арифметичне лише числових елементів даного масиву.
+
+// const array = [24, 'арифметичне', 65, 'даного', 43, 'x', 12, undefined, NaN, {x: true, y: false}];
+// const arithmeticMean = function calcArithmeticMean(arr){
+//     const arrNumbers = arr.filter(currentValue => (!(isNaN(+currentValue))));
+//     const result = arrNumbers.reduce((accum, elArr) =>  accum += elArr / arrNumbers.length, 0)
+//     return result
+// };
+// console.log(arithmeticMean(array))
+
+
+
+
+// Написати функцію заповнення даними користувача двомірного масиву.
+//  Довжину основного масиву і внутрішніх масивів задає користувач.
+//  Значення всіх елементів всіх масивів задає користувач.
+
+// function dataFillTwoDimArray() {
+//     let twoDimArray = [];
+//     let lengthMainArray = +prompt("Введіть довжину основного масиву");
+//     for (i = 0; i < lengthMainArray; i++) {
+//         let elMainArray = prompt(`Введіть ${i + 1}-ий елемент основного масиву. Якщо хочете створити внутрішній масив введіть [] `);
+//         if (elMainArray === "[]") {
+//             let lengthIntArray;
+//             twoDimArray[i] = [];
+//             lengthIntArray = +prompt(`Введіть довжину внутрішнього масиву`);
+//             for (j = 0; j < lengthIntArray; j++) {
+//                 let elIntArray = prompt(`Введіть ${j + 1}-ий елемем внутрішнього масиву`);
+//                 if (!isNaN(+elIntArray)) {
+//                     twoDimArray[i].push(+elIntArray);
+//                 } else {
+//                     twoDimArray[i].push(elIntArray);
+//                 }
+//             } 
+//         } else if (!isNaN(+elMainArray)) {
+//             twoDimArray.push(+elMainArray);
+//         } else {
+//             twoDimArray.push(elMainArray);
+//         }
+//     }
+//     return twoDimArray
+// }
+// const result = dataFillTwoDimArray();
+// console.log(result)
+
+
+
+
+// -Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor".
+//  Вихідний рядок та символи для видалення задає користувач
+
+// const getString = prompt('Введіть рядок');
+// const getSymbolsRemove = prompt('Введіть символи для видалення')
+// const arraySymbols = getSymbolsRemove === null ? [] : getSymbolsRemove.split("");
+
+// function helloWorld (string, arr) {
+//         let result = string.split('');
+//         for(let i = 0; i<arr.length; i++){
+//             for(let j = 0; j<string.length; j++) {
+//                 if(arr[i] === string[j]){
+//                     result.splice(result.indexOf(arr[i]),1)
+//                 };
+//             }
+//         }
+//         return result.join('');
+
+// };
+// alert(helloWorld(getString, arraySymbols));
