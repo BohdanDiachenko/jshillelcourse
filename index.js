3a2750f44ccdecdf50e7d00bd79b786cb3a5d2f2
wBogdaNw <bogdanid91@gmail.com>
2022-11-01 17:25:12 +0100

    ww

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 // =========  Написати функцію, яка приймає 1 параметр. з тим, що передали перший раз і т. д.
 // =========  Все це із замиканнями, наприклад: sum(3) = 3 sum(5) = 8 sum(20) = 28
 // =========================================================================================
-function count() {
-    let sum = 0;
-    return function (number) {
-        sum += number;
-        return sum;
-    };
-}
-const sum = count();
+// function count() {
+//     let sum = 0;
+//     return function (number) {
+//         sum += number;
+//         return sum;
+//     };
+// }
+// const sum = count();
 
-console.log(sum(3));
-console.log(sum(5));
-console.log(sum(20));
+// console.log(sum(3));
+// console.log(sum(5));
+// console.log(sum(20));
