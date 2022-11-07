84a585382acd5104600bdcc9dc7422b16cd1760e
wBogdaNw <bogdanid91@gmail.com>
2022-11-07 19:56:07 +0100

    hw22

--- index.js
+++ index.js
@@ -1,168 +1,32 @@
-
-let numOrStr = prompt("input number or string");
-console.log(numOrStr);
-
-switch (isNaN(numOrStr) || numOrStr) {
-    case null:
-        console.log("ви скасували");
-
-        break;
-
-    case "":
-        console.log("Empty String");
-
-        break;
-
-    case true:
-        console.log(" number is Ba_NaN");
-
-        break;
-
-    default:
-        console.log("OK!");
-
-        break;
-}
+const container = document.createElement('div');
+container.style.cssText = `max-width: 1200px; height: 100vh; margin: 0 auto; background-color: #76d5cc3d; font-size: 20px; display: flex; flex-direction: column; justify-content: center;align-items: center;`
+document.body.prepend(container);
 
 
-// let arithmeticOperations = prompt('що ви хочете зробити(add, sub, mult, div)?');
-// let result;
-// if(arithmeticOperations === 'add' || arithmeticOperations === 'sub' || arithmeticOperations === 'mult' || arithmeticOperations === 'div'){
-//     let oneNumber = +(prompt('введіть перше число'));
-//     let twoNumber = +(prompt('введіть друге число'));
-//     if(arithmeticOperations === 'add'){
-//         arithmeticOperations = '+';
-//         result = oneNumber + twoNumber;
-//     };
-//     if (arithmeticOperations === 'sub'){
-//         arithmeticOperations = '-';
-//         result = oneNumber - twoNumber;
-//     };
-//     if (arithmeticOperations === 'mult'){
-//         arithmeticOperations = '*';
-//         result = oneNumber * twoNumber;
-//     };  
-//     if (arithmeticOperations === 'div'){
-//         arithmeticOperations = '/';
-//         result = oneNumber / twoNumber;
-//     };
-//     alert(`${oneNumber} ${arithmeticOperations} ${twoNumber} = ${result}`);
-// } 
-// else {
-//     alert('читай умову');
-// };
-
-
-
-// let arithmeticOperations;
-// let oneNumber;
-// let twoNumber
-// switch (arithmeticOperations = prompt('що ви хочете зробити(add, sub, mult, div)?')) {
-
-//     case a = "add":
-//         oneNumber = +(prompt('введіть перше число'));
-//         twoNumber = +(prompt('введіть друге число'));
-//         alert(`${oneNumber} + ${twoNumber} = ${oneNumber + twoNumber}`);
-//         break;
-
-//     case "sub":
-//         oneNumber = +(prompt('введіть перше число'));
-//         twoNumber = +(prompt('введіть друге число'));
-//         alert(`${oneNumber} - ${twoNumber} = ${oneNumber - twoNumber}`);
-//         break;
-
-//     case "mult":
-//         oneNumber = +(prompt('введіть перше число'));
-//         twoNumber = +(prompt('введіть друге число'));
-//         alert(`${oneNumber} * ${twoNumber} = ${oneNumber * twoNumber}`);;
-//         break;
-
-//     case "div":
-//         oneNumber = +(prompt('введіть перше число'));
-//         twoNumber = +(prompt('введіть друге число'));
-//         alert(`${oneNumber} / ${twoNumber} = ${oneNumber / twoNumber}`);;
-//         break;
-        
-//     default:
-//         alert("Читай умову!");
-//         break;
-// };
-
-// let firstNumber = +prompt('введіть перше число');
-// let secondNumber = +prompt('введіть друге число');
-// let add = `${firstNumber}+${secondNumber} = ${firstNumber + secondNumber}`;
-// let sub = `${firstNumber}-${secondNumber} = ${firstNumber - secondNumber}`;
-// let mult = `${firstNumber}*${secondNumber} = ${firstNumber * secondNumber}`;
-// let div = `${firstNumber}/${secondNumber} = ${firstNumber / secondNumber}`;
-// let resolt = `Ваш результат:\n${add}\n${sub}\n${mult}\n${div}`;
-// alert(resolt);
-
-
-//homework2/1
-
-// let surName = prompt ('Enter your last name');
-
-// let firstName = prompt('Enter your firs name');
-
-// let patronymic = prompt('Enter your middle name');
-
-// let greeting = 'good morning ' + surName + ' ' + firstName + ' ' + patronymic;
-// // let greeting = `Good Morning ${surName} ${firstName} ${patronymic}`;
-
-
-// alert(greeting);
-
-
-
-
-// HomeWork2/2
-
-// let number =  92358;
-// let string = String(number);
-// let newString = '';
-// for(i = 0; i < string.length ; i++){
-//     newString += `${string[i]} `;
-// };
-// console.log(newString);
-=======
-let firstNumber = +prompt('введіть перше число');
-let secondNumber = +prompt('введіть друге число');
-let thirdNumber = +prompt('введіть третє число');
-let arithmeticMean = `${(firstNumber+secondNumber+thirdNumber)/3}`;
-alert(arithmeticMean);
-
-const SECONDS_IN_HOUR = 3600;
-let hours = +prompt('введіть кількість годин які хочете перевести в секунди');
-let seconds = `${hours * SECONDS_IN_HOUR} секунд`;
-alert(seconds);
-
-let condition = false;
-let getUserLengthArray;
-do {
-    getUserLengthArray = prompt("Введіть довжину масиву");
-    if (isNaN(getUserLengthArray) === true) {
-        //return "Please imput a number.";
-        alert("Будь ласка, введіть число");
-        condition = false;
-    } else {
-        condition = true;
-    }
-} while (condition === false);
-
-const newArray = [];
-for (let i = 0; i < getUserLengthArray; i++) {
-    newArray[i] = prompt(`Введіть елемент масиву № ${i + 1}`);
-    if (newArray[i] === "" || newArray[i] === null) {
-        delete newArray[i];
-    } else if (!isNaN(+newArray[i])) {
-        newArray[i] = Number(newArray[i]);
+const table = function(row, column, startingNumber){
+    let currentNumber = startingNumber
+    const table = document.createElement('table');
+    table.style.cssText = 'border-collapse: collapse; border: 3px solid; margin: 10px 0 30px; background-color: #d72fc91e;'
+    container.prepend(table)
+    const title = document.createElement('h1')
+    title.innerText = `Таблиця від ${currentNumber} до ${row*column+currentNumber-1}`
+    title.style.cssText = `color: #f16b11`
+    table.before(title)
+    if (currentNumber <= row*column){
+        for(i = 0; i < row; i++){
+            const tr = document.createElement('tr');
+            table.append(tr);
+            for( j = 0; j < column; j++){
+                const td = document.createElement('td');
+                tr.append(td)
+                td.innerText = `${currentNumber}`;
+                currentNumber++
+                td.style.cssText = 'border: 3px solid #6ccbe7; text-align: center; padding: 10px 15px; color: #ff6851';
+            }
+        }
     }
 }
-
-console.log(newArray);
-console.log(newArray.sort().sort((a, b) => a - b));
-newArray.splice(1, 3);
-console.log(newArray);
-
-
-
+const row = 10;
+const column = 10;
+const startingNumber = 1;
+table(row, column, startingNumber)
