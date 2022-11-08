43908fab77787f40a743b2971eb5b284d1f73bb1
wBogdaNw <bogdanid91@gmail.com>
2022-11-08 16:09:28 +0100

    addition

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ wrapper.prepend(img)
 
 
 const imagesArray = [
-    {src: 'image/dog1.jpeg', alt: 'dog1'},
-    {src: 'image/dog2.jpeg', alt: 'dog2'},
-    {src: 'image/dog3.jpeg', alt: 'dog3'},
-    {src: 'image/dog4.jpeg', alt: 'dog4'},
-    {src: 'image/dog5.jpeg', alt: 'dog5'},
-    {src: 'image/dog6.jpeg', alt: 'dog6'},
+    {src: './image/dog1.jpeg', alt: 'dog1'},
+    {src: './image/dog2.jpeg', alt: 'dog2'},
+    {src: './image/dog3.jpeg', alt: 'dog3'},
+    {src: './image/dog4.jpeg', alt: 'dog4'},
+    {src: './image/dog5.jpeg', alt: 'dog5'},
+    {src: './image/dog6.jpeg', alt: 'dog6'},
 ];
 
 const randomImage = (function () {
