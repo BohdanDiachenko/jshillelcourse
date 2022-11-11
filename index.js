f76e50120e05dc9cc66be5a6d0c87300ed01d4e8
wBogdaNw <bogdanid91@gmail.com>
2022-11-11 09:07:46 +0100

    hw24

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// У папці `img` є зображення.
+// Пишемо свій слайдер зображень.
 
-// При кожному завантаженні сторінки повинно виводитися випадково обране зображення.
+// На сторінці є зображення та кнопки Next, Prev з боків від зображення.
 
+// При кліку на Next - показуємо наступне зображення.
 
+// При кліку на Prev - попереднє.
 
-const wrapper = document.createElement('div');
-wrapper.className = 'wrapper';
-wrapper.style.cssText = 'display: grid; place-items: center; height: 100vh;'
-document.body.prepend(wrapper);
+// При досягненні останнього зображення - ховати кнопку Next. Аналогічно з першим зображенням і кнопкою Prev.
 
-const img = document.createElement('img');
-img.style.width = '800px';
-wrapper.prepend(img)
-
+const prev = document.getElementById ('prev');
+const next = document.getElementById ('next');
+const img = document.getElementById ('image');
 
 const imagesArray = [
     {src: 'image/dog1.jpeg', alt: 'dog1'},
@@ -23,10 +21,32 @@ const imagesArray = [
     {src: 'image/dog6.jpeg', alt: 'dog6'},
 ];
 
-const randomImage = (function () {
-    const randomNumber = Math.floor(Math.random() * imagesArray.length);
-    return imagesArray[randomNumber];
-}) ();
+let a = 0;
 
-img.setAttribute('src', randomImage.src);
-img.setAttribute('alt', randomImage.alt);
+prev.addEventListener('click', () => {
+    a--
+    if (a === imagesArray.length - 2) {
+        next.style.visibility = "visible";
+    }
+
+    img.setAttribute('src', imagesArray[a].src);
+    img.setAttribute('alt', imagesArray[a].alt);
+
+    if (a === 0 ) {
+        prev.style.visibility = 'hidden';
+    }
+}); 
+
+next.addEventListener('click', () => {
+    a++
+    if (a > 0) {
+        prev.style.visibility = "visible";
+    }
+
+    img.setAttribute('src', imagesArray[a].src);
+    img.setAttribute('alt', imagesArray[a].alt);
+
+    if (a === imagesArray.length - 1) {
+        next.style.visibility = 'hidden';
+    }
+});
